6b7d091d6e66aa93a6111bf3b3e51f1c963a8eb0
Steven Bakhtiari <steven@substrate.co.uk>
2019-12-17 15:58:22 +0000

    use a more type-safe solution

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -5,22 +5,30 @@ object FizzBuzz {
     s"${(fizzBuzzed).mkString(" ")} ${report(fizzBuzzed)}"
   }
 
-  private def report(fizzBuzzed: Seq[Any]) = {
+  private def report(fizzBuzzed: Seq[FizzBuzzResult]) = {
     val counts = fizzBuzzed.groupBy {
-      case v: Int => "integer"
+      case v: Number => Number
       case v => v
     }.view.mapValues(_.size)
     
-    List("fizz", "buzz", "fizzbuzz", "lucky", "integer")
-      .map(k => s"$k: ${counts.getOrElse(k, 0)}")
-      .mkString(" ")
+    Seq(Fizz, Buzz, FizzBuzz, Lucky, Number).map {
+      case k@Number => s"integer: ${counts.getOrElse(k, 0)}"
+      case k => s"$k: ${counts.getOrElse(k, 0)}"
+    }.mkString(" ")
   }
 
-  private val fizzBuzzTheNumber: PartialFunction[Int, Any] = {
-    case n if n.toString.contains("3") => "lucky"
-    case n if n % 15 == 0 => "fizzbuzz"
-    case n if n % 3 == 0 => "fizz"
-    case n if n % 5 == 0 => "buzz"
-    case n => n
+  private val fizzBuzzTheNumber: PartialFunction[Int, FizzBuzzResult] = {
+    case n if n.toString.contains("3") => Lucky
+    case n if n % 15 == 0 => FizzBuzz
+    case n if n % 3 == 0 => Fizz
+    case n if n % 5 == 0 => Buzz
+    case n => Number(n)
   }
+
+  sealed trait FizzBuzzResult
+  case class Number(value: Int) extends FizzBuzzResult { override def toString = value.toString }
+  case object Fizz extends FizzBuzzResult { override def toString = "fizz" }
+  case object Buzz extends FizzBuzzResult { override def toString = "buzz" }
+  case object FizzBuzz extends FizzBuzzResult { override def toString = "fizzbuzz" }
+  case object Lucky extends FizzBuzzResult { override def toString = "lucky" }
 }
